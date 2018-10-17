7696ca91520d0180814280a4699bf000251cbb5c
Torbjørn Frotveit <torbjorn.frotveit@inmeta.no>
2018-10-17 08:56:41 +0200

    Update .net version

--- FileNamerCore/DataTypes/FileInformation.cs
+++ FileNamerCore/DataTypes/FileInformation.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileNamerCore.DataTypes
 {
@@ -22,7 +18,6 @@ namespace FileNamerCore.DataTypes
             CreationTime = info.CreationTime;
             ChangedTime = info.LastWriteTime;
         }
-
 
     }
 }
